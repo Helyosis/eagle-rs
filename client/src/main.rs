4ca862b09ff5e1e2e823539a5b17bba56f17448e
Aloïs Colléaux-Le Chêne <alois.colleaux-le-chene@epita.fr>
2024-11-08 03:01:11 -0800

    fix: update dependencies
    ntapi v0.3.7 fails to build on recent version of Windows (?). Updating it fixes it.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,6 @@
-use sysinfo::{Pid, SystemExt, ProcessExt};
+use sysinfo::Pid;
 use winapi::um::{fileapi::{CreateFileA, OPEN_EXISTING}, winnt::{GENERIC_READ, GENERIC_WRITE, FILE_SHARE_READ, FILE_SHARE_WRITE}, handleapi::CloseHandle};
-use std::{ffi::CString, ptr::null_mut};
+use std::{ffi::{CString, OsStr}, ptr::null_mut};
 mod kernel_interface;
 use clap::{Args, Parser, Subcommand, ArgGroup};
 
@@ -117,7 +117,7 @@ fn main() {
 
     match &cli.command {
         Commands::Process(p) => {
-            let process_id = get_process_id_by_name(p.name.as_str()) as u32;
+            let process_id = get_process_id_by_name(p.name.as_str()).as_u32();
             
             if p.protect {
                 kernel_interface::protect_process(process_id, driver_handle);
@@ -168,9 +168,9 @@ fn get_process_id_by_name(target_process: &str) -> Pid {
     let mut system = sysinfo::System::new();
     system.refresh_all();
 
-    let mut process_id = 0;
+    let mut process_id = Pid::from(0);
 
-    for process in system.process_by_name(target_process) {
+    for process in system.processes_by_name(OsStr::new(target_process)) {
         process_id = process.pid();
     }
 
